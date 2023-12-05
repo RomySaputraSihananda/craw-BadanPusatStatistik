625cdebc084312e419aca7bdcff92a4ab05fe00c
romys <romysaputrasihanandaa@gmail.com>
2023-12-05 23:15:18 +0700

    update LICENCE

--- README.md
+++ README.md
@@ -6,18 +6,49 @@
 
 craw-BadanPusatStatistik adalah program untuk mengambil data dari website Badan Pusat Statistik Indonesia.
 
-## Installation
-
 ## Requirements
 
-- **Flask**>=2.1.0
-- **flask_restx**>=0.5.1
-- **gevent**>=23.9.0.post1
-- **pyquery**>=2.0.0
-- **pytz**>=2023.3.post1
-- **Requests**>=2.31.0
+- **Python** >= 3.11.4
+- **Flask** >= 2.1.0
+- **flask_restx** >= 0.5.1
+- **gevent** >= 23.9.0.post1
+- **pyquery** >= 2.0.0
+- **pytz** >= 2023.3.post1
+- **Requests** >= 2.31.0
+
+## Installation
+
+```sh
+# Clonig Repository
+git clone https://github.com/romysaputrasihananda/craw-BadanPusatStatistik
+
+# Change Current Working Directory
+cd craw-BadanPusatStatistik;
+
+# Install Requirement Dependency Modules
+pip install -r requirements.txt
+```
 
 ## Example Usages
+
+### CLI
+
+```sh
+# Sosial dan Kependudukan -> Sosial
+python main.py --topic=Sosial
+
+# Ekonomi dan Perdagangan  -> Ekonomi
+python main.py --topic=Ekonomi
+
+# Pertanian dan Pertambangan -> Pertanian
+python main.py --topic=Pertanian
+```
+
+### Swagger-ui
+
+```sh
+python main.py --server=true
+```
 
 ## License
 
--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from lib.helpers.TypeEnums import Sosial_Kependudukan, Ekonomi_Perdagangan, Pert
 import os; 
 
 def loop_write(main: str, options: any) -> None:
-    for option in options:
-        data = dumps(bps.execute(option.value));
+    for topic in options:
+        data = dumps(bps.execute(topic.value));
 
-        path = f'data/{main}/{option.name}.json'
+        path = f'data/{main}/{topic.name}.json'
 
         if(not os.path.exists('/'.join(path.split('/')[:-1]))):
             os.makedirs('/'.join(path.split('/')[:-1]));
@@ -24,7 +24,7 @@ def loop_write(main: str, options: any) -> None:
 
 if __name__ == "__main__":
     argp = argparse.ArgumentParser();
-    argp.add_argument("option", type=str);
+    argp.add_argument("--topic", type=str);
     argp.add_argument("--server", type=bool);
     argp.add_argument("--port", type=int, default=4444);
     args = argp.parse_args();
@@ -42,17 +42,20 @@ if __name__ == "__main__":
         http_server = WSGIServer(("localhost", args.port), application);
         http_server.serve_forever();
 
-    from lib.services import Bps;
+    if(args.topic):
+        from lib.services import Bps;
     
-    bps: Bps = Bps();
+        bps: Bps = Bps();
 
-    match(args.option.upper()):
-        case 'SOSIAL':
-            loop_write('Sosial', Sosial_Kependudukan);
-        case 'EKONOMI':
-            loop_write('Ekonomi', Ekonomi_Perdagangan);
-        case 'PERTANIAN':
-            loop_write('Pertanian', Pertanian_Pertambangan);
+        match(args.topic.upper()):
+            case 'SOSIAL':
+                loop_write('Sosial', Sosial_Kependudukan);
+            case 'EKONOMI':
+                loop_write('Ekonomi', Ekonomi_Perdagangan);
+            case 'PERTANIAN':
+                loop_write('Pertanian', Pertanian_Pertambangan);
+
+    print('ok')
 
 
 
